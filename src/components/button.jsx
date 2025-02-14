51322753b62856add0d297918703b07e8ce8dce5
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-02-14 13:48:09 +0530

    Completed the assignment

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,19 @@
 // write button card here
+import React from "react";
+
+const ViewButton = () => {
+    const buttonStyle = {
+    backgroundColor: "grey",
+    color: "white",
+    padding: "8px 12px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "14px",
+    marginTop: "10px",
+    };
+
+    return <button style={buttonStyle}>View Product</button>;
+};
+
+export default ViewButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,34 @@
 // write product card here
+import React from "react";
+import ViewButton from "./button";
+
+const Productcard = () => {
+    const productImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrAPkJr-A83pgKDpNBrRatIXniBLXptZhmZQ&s";
+    const productName = "iPhone 16 Pro";
+    const price = "$1500";
+
+    const cardStyle = {
+    border: "2px solid #ddd",
+    borderRadius: "10px",
+    padding: "15px",
+    textAlign: "center",
+    width: "200px"
+    };
+
+    const imgStyle = {
+    width: "100px",
+    height: "100px",
+    marginBottom: "10px",
+    };
+
+    return (
+    <div style={cardStyle}>
+        <img src={productImage} alt="product" style={imgStyle} />
+        <h3>{productName}</h3>
+        <p>{price}</p>
+        <ViewButton />
+        </div>
+    );
+};
+
+export default Productcard;
